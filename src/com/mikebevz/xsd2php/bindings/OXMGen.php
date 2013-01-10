210922a77ab0064a2ca46b9922a0109eaea5e220
Alistair MacDonald <Alistair.MacDonald@illumina.co.uk>
2013-01-10 10:16:21 +0000

    OPEN - issue YZABP-125: Some fields do not get written out on serialization  https://illuminadev.co.uk/jira/browse/YZABP-125
    Attributes should always be handled as such, regardless of whether they
    are originally simple.
    
    Elements that will be handled as simple types (ie real simple types or
    XMLSchema mapped elements) must be processed as XmlText in order to
    actually retrieve the value (otherwise they need to be full classes)

--- src/com/mikebevz/xsd2php/bindings/OXMGen.php
+++ src/com/mikebevz/xsd2php/bindings/OXMGen.php
@@ -53,7 +53,13 @@ class OXMGen {
       $modifiers['name'] = $phpProperty->myClass->name;
       $phpProperty->docBlock->XmlValue = static::docBlockPropertyModifiers($modifiers);
     }
-    elseif ($phpProperty->simpleType) {
+    elseif (@$phpProperty->xmlType == 'attribute') {
+      #println($phpProperty->type, 'XmlAttribute: ' . $modifiers['type']);
+      $modifiers['name'] = $phpProperty->name;
+      $modifiers['type'] = $phpProperty->phpType; // Must use the simple PHP Type but that was done earlier
+      $phpProperty->docBlock->XmlAttribute = static::docBlockPropertyModifiers($modifiers);
+    }
+    elseif ($phpProperty->simpleType || strpos($modifiers['type'], '\\') === FALSE) {
       #println($phpProperty->type, 'XmlText: ' . $modifiers['type']);
       $modifiers['name'] = $phpProperty->name;
       // Can use any type but must be normalized if not namespaced
@@ -61,12 +67,6 @@ class OXMGen {
 	    $modifiers['type'] = $phpProperty->phpType;
       } 
       $phpProperty->docBlock->XmlText = static::docBlockPropertyModifiers($modifiers);
-    }
-    elseif (@$phpProperty->xmlType == 'attribute') {
-      #println($phpProperty->type, 'XmlAttribute: ' . $modifiers['type']);
-      $modifiers['name'] = $phpProperty->name;
-      $modifiers['type'] = $phpProperty->phpType; // Must use the simple PHP Type but that was done earlier
-      $phpProperty->docBlock->XmlAttribute = static::docBlockPropertyModifiers($modifiers);
     }
     else {
       #println($phpProperty->type, 'XmlElement: ' . $modifiers['type']);
